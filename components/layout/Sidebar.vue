6382353db2b15dada7f643e47fa9febd08e923a5
Dexstager <ravtec123@gmail.com>
2023-10-27 13:40:35 -0600

    changes

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,172 @@
+<template>
+    <div :class="{ 'dark text-white-dark': store.semidark }">
+        <nav class="sidebar fixed top-0 bottom-0 z-50 h-full min-h-screen w-[260px] shadow-[5px_0_25px_0_rgba(94,92,154,0.1)] transition-all duration-300">
+            <div class="h-full bg-white dark:bg-[#0e1726]">
+                <div class="flex items-center justify-between px-4 py-3">
+                    <NuxtLink to="/" class="main-logo flex shrink-0 items-center">
+                        <img class="ml-[5px] w-8 flex-none" src="/assets/images/logo.svg" alt="" />
+                        <span class="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">Rentals</span>
+                    </NuxtLink>
+                    <a
+                        href="javascript:;"
+                        class="collapse-icon flex h-8 w-8 items-center rounded-full transition duration-300 hover:bg-gray-500/10 hover:text-primary rtl:rotate-180 dark:text-white-light dark:hover:bg-dark-light/10"
+                        @click="store.toggleSidebar()"
+                    >
+                        <svg class="m-auto h-5 w-5" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13 19L7 12L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            <path
+                                opacity="0.5"
+                                d="M16.9998 19L10.9998 12L16.9998 5"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                    </a>
+                </div>
+                <client-only>
+                    <perfect-scrollbar
+                        :options="{
+                            swipeEasing: true,
+                            wheelPropagation: false,
+                        }"
+                        class="relative h-[calc(100vh-80px)]"
+                    >
+                        <ul class="relative space-y-0.5 p-4 py-0 font-semibold">
+                            <li class="menu nav-item">
+                                <button
+                                    type="button"
+                                    class="nav-link group w-full"
+                                    :class="{ active: activeDropdown === 'dashboard' }"
+                                    @click="activeDropdown === 'dashboard' ? (activeDropdown = null) : (activeDropdown = 'dashboard')"
+                                >
+                                    <div class="flex items-center">
+                                        <svg
+                                            class="group-hover:!text-primary shrink-0"
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                opacity="0.5"
+                                                d="M2 12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274C22 8.77128 22 9.91549 22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M9 17.25C8.58579 17.25 8.25 17.5858 8.25 18C8.25 18.4142 8.58579 18.75 9 18.75H15C15.4142 18.75 15.75 18.4142 15.75 18C15.75 17.5858 15.4142 17.25 15 17.25H9Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+
+                                        <span class="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">
+                                            {{ $t('dashboard') }}
+                                        </span>
+                                    </div>
+                                    <div class="rtl:rotate-180" :class="{ '!rotate-90': activeDropdown === 'dashboard' }">
+                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M9 5L15 12L9 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                    </div>
+                                </button>
+                                <vue-collapsible :isOpen="activeDropdown === 'dashboard'">
+                                    <ul class="sub-menu text-gray-500">
+                                        <li>
+                                            <NuxtLink to="/" @click="toggleMobileMenu"> Home </NuxtLink>
+                                        </li>
+                                        <li>
+                                            <NuxtLink to="#" @click="toggleMobileMenu">{{ $t('analytics') }}</NuxtLink>
+                                        </li>
+                                        <li>
+                                            <NuxtLink to="#" @click="toggleMobileMenu">{{ $t('finance') }}</NuxtLink>
+                                        </li>
+                                        <li>
+                                            <NuxtLink to="#" @click="toggleMobileMenu">{{ $t('crypto') }}</NuxtLink>
+                                        </li>
+                                    </ul>
+                                </vue-collapsible>
+                            </li>
+    
+ 
+ 
+  
+  
+  
+ 
+
+                            <li class="menu nav-item">
+                                <a href="#" target="_blank" class="nav-link group">
+                                    <div class="flex items-center">
+                                        <svg
+                                            class="group-hover:!text-primary shrink-0"
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                clip-rule="evenodd"
+                                                d="M4 4.69434V18.6943C4 20.3512 5.34315 21.6943 7 21.6943H17C18.6569 21.6943 20 20.3512 20 18.6943V8.69434C20 7.03748 18.6569 5.69434 17 5.69434H5C4.44772 5.69434 4 5.24662 4 4.69434ZM7.25 11.6943C7.25 11.2801 7.58579 10.9443 8 10.9443H16C16.4142 10.9443 16.75 11.2801 16.75 11.6943C16.75 12.1085 16.4142 12.4443 16 12.4443H8C7.58579 12.4443 7.25 12.1085 7.25 11.6943ZM7.25 15.1943C7.25 14.7801 7.58579 14.4443 8 14.4443H13.5C13.9142 14.4443 14.25 14.7801 14.25 15.1943C14.25 15.6085 13.9142 15.9443 13.5 15.9443H8C7.58579 15.9443 7.25 15.6085 7.25 15.1943Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                opacity="0.5"
+                                                d="M18 4.00038V5.86504C17.6872 5.75449 17.3506 5.69434 17 5.69434H5C4.44772 5.69434 4 5.24662 4 4.69434V4.62329C4 4.09027 4.39193 3.63837 4.91959 3.56299L15.7172 2.02048C16.922 1.84835 18 2.78328 18 4.00038Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+
+                                        <span class="text-black ltr:pl-3 rtl:pr-3 dark:text-[#506690] dark:group-hover:text-white-dark">{{
+                                            $t('documentation')
+                                        }}</span>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </perfect-scrollbar>
+                </client-only>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { ref, onMounted } from 'vue';
+
+    import { useAppStore } from '@/stores/index';
+    import VueCollapsible from 'vue-height-collapsible/vue3';
+    const store = useAppStore();
+    const activeDropdown: any = ref('');
+    const subActive: any = ref('');
+
+    onMounted(() => {
+        setTimeout(() => {
+            const selector = document.querySelector('.sidebar ul a[href="' + window.location.pathname + '"]');
+
+            if (selector) {
+                selector.classList.add('active');
+                const ul: any = selector.closest('ul.sub-menu');
+                if (ul) {
+                    let ele: any = ul.closest('li.menu').querySelectorAll('.nav-link') || [];
+
+                    if (ele.length) {
+                        ele = ele[0];
+                        setTimeout(() => {
+                            ele.click();
+                        });
+                    }
+                }
+            }
+        });
+    });
+
+    const toggleMobileMenu = () => {
+        if (window.innerWidth < 1024) {
+            store.toggleSidebar();
+        }
+    };
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Tailwind Template',
+            titleTemplate: '%s',
+            htmlAttrs: {
+                lang: 'en',
+            },
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no',
+                },
+                { hid: 'description', name: 'description', content: '' },
+                { name: 'format-detection', content: 'telephone=no' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap',
+                },
+            ],
+        },
+    },
+
+    css: ['~/assets/css/app.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    modules: ['@pinia/nuxt', '@nuxtjs/i18n'],
+
+    i18n: {
+        locales: [
+             { code: 'en', file: 'en.json' },
+            ],
+        lazy: true,
+        defaultLocale: 'en',
+        strategy: 'no_prefix',
+        langDir: 'locales/',
+    },
+    vite: {
+        optimizeDeps: { include: ['quill'] },
+    },
+    router: {
+        options: { linkExactActiveClass: 'active' },
+    },
+});
